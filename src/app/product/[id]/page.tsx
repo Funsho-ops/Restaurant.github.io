3dd2af39ce7fe682cb8b637567fcf468ad82a599
Funsho Akinbile <81854730+Funsho-ops@users.noreply.github.com>
2023-10-19 21:56:41 +0100

    New Updates

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,9 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
 
 const SingleProductPage = () => {
   return (
-    <div>
-      
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-red-500 md:flex-row'>
+      {/* IMAGE CONTAINER */}
+      <div className='relative'>
+          <Image src="" alt='' className='object-contain' fill/> 
+      </div>
+      {/* IMAGE CONTAINER */}
     </div>
   )
 }
